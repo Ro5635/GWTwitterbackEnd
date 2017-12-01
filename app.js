868441fe488c0ec221256f15b678d5a66430c333
Robert Curran <Ro5635@users.noreply.github.com>
2017-12-01 16:46:13 +0000

    Add CORS
    Add CORS Middleware

--- app.js
+++ app.js
@@ -13,6 +13,14 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.setHeader('X-Powered-By', 'Manx Cats');
+    next();
+});
+
+
 app.use('/', index);
 
 // catch 404 and forward to error handler
