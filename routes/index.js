ae0cae1fd0f36b17ced6a0d17aff5ba17a0b81c7
Alex Wiley <alex@alexwiley.co.uk>
2017-12-01 16:29:52 +0000

    Updated server

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,23 @@
 "use strict";
 const express = require('express');
 const router = express.Router();
+const secret = require("secret");
+Twitter = require('node-twitter-api'),
 
 // Custom models
 const tweets = require('../models/tweets');
+
+let twitter = new Twitter({
+    consumerKey: 'EoyqbOuKhTRDgfe7cpuZx2TlG',
+    consumerSecret: 'mQfLhgXOg2SLqTCYKs7VNf7bjStVMgdOA8GweP6AtClbryEMtX',
+    callback: baseURL + '/auth'
+});
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 router.get('/', function(req, res, next) {
 
@@ -24,5 +38,51 @@ router.get('/', function(req, res, next) {
     });
 
 });
+
+
+app.get('/twitter', (req, res) => {
+    // res.send(JSON.stringify({text: 'something here...'}))
+    twitter.getRequestToken((err, requestToken, requestSecret) => {
+        if(err){
+            res.status(500).send(err);
+            return;
+        }
+        secret.set('token', requestToken);
+        secret.set('secret', requestSecret);
+        res.send({
+            token: requestToken,
+        })
+    })
+});
+
+
+app.get('/access-token', (req, res) => {
+    let verifier = req.query.verifier,
+       token = req.query.token;
+
+    // res.send({verifier, token});
+    twitter.getAccessToken(token, secret.get('secret'), verifier, (err, accessToken, accessSecret) => {
+        if(err) return res.status(500).send(err);
+        // res.send({accessToken, accessSecret});
+        twitter.verifyCredentials(accessToken, accessSecret, (err, user) => {
+            console.log(accessToken);
+            let ret = {
+                token: accessToken,
+                user: user
+            };
+            res.send(ret);
+        })
+    })
+
+});
+
+
+let mock = require('./mock.json');
+
+app.get('/test-data', (req, res) => {
+    res.send(mock);
+});
+
+
 
 module.exports = router;
